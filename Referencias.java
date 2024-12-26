9ba74308745423c4c0e12785092d551e33059b46
jehlp3 <jessicalacerdap@hotmail.com>
2024-12-26 17:24:36 -0300

    Modelagem

---
+++ Referencias.java
@@ -0,0 +1,29 @@
+public class Referencias {
+    private String converterDe;
+    private String converterPara;
+    private double quantia;
+
+    public String getConverterDe() {
+        return converterDe;
+    }
+
+    public void setConverterDe(String converterDe) {
+        this.converterDe = converterDe;
+    }
+
+    public String getConverterPara() {
+        return converterPara;
+    }
+
+    public void setConverterPara(String converterPara) {
+        this.converterPara = converterPara;
+    }
+
+    public double getQuantia() {
+        return quantia;
+    }
+
+    public void setQuantia(double quantia) {
+        this.quantia = quantia;
+    }
+}
