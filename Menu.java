620d96a92d7b5a73fbe8333809b4065c2a6861fb
jehlp3 <jessicalacerdap@hotmail.com>
2024-12-26 17:24:14 -0300

    Menu

---
+++ Menu.java
@@ -0,0 +1,19 @@
+
+public class Menu {
+    public void opcoesDeMoedas(){
+        System.out.println("""
+                 ***********************************************************************
+                                       SELECIONE A MOEDA                             \s
+                 ***********************************************************************\n
+                 (1) - USD - Dólar Americano
+                 (2) - EUR - Euro
+                 (3) - BRL - Real Brasileiro
+                 (4) - GBP - Libra Esterlina
+                 (5) - JPY - Iene
+                 (6) - ARS - Peso Argentino
+                 (7) - CLP - Peso Chileno  \n
+                 ***********************************************************************
+                       \s""");
+
+    }
+}
